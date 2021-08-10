d364751b9be001627dc44662c1c8bf858adf4f7b
kiwislob <kiwislob@gmail.com>
2021-08-10 20:31:04 +1200

    move stubs

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ echo "Building stubs"
 stubgen "$root/returns" -o "$root/stubs"
 echo "Downgrading source files"
 3to2 --write --no-diffs "$root/returns"
-
+echo "Moving stubs beside source files"
+cp -RT "$root/stubs/returns" "$root/returns"
+rm -R "$root/stubs"
